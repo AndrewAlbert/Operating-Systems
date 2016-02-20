52d1c200d60663dc5aea02b4b360cd6c9341d2ce
AndrewAlbert <andrew.albert@att.net>
2016-02-20 12:32:15 -0500

    Commit Lab3-1 and Lab3-2 which deal with shared data between two kthreads. Lab3-2 also incorporates a semaphore to lock access to this shared data

---
+++ Lab3-1.c
@@ -0,0 +1,67 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/kthread.h>
+
+//two kthreads that will share access to data
+struct task_struct *t1, *t2;
+
+//
+int arr[1000000];
+int idx = 0;
+//total number of increments done by each kthread
+int id1 = 1, id2 = 2;
+int cs1 = 0, cs2 = 0;
+//array to total the number of single, double, triple, etc. increments that were done in arr
+int stat[1000000];
+
+int incrementer(void *ptr){
+	int id = *(int*)ptr;
+	printk(KERN_INFO "Consumer TID %d\n", id);
+	//increment every value of arr[idx] in each respective kthread if the semaphore is currently unlocked. 
+	//If it is, lock semaphore until arr[idx'] has been incremented.
+	while (idx < 1000000 && !kthread_should_stop()){
+		arr[idx++] += 1;
+		if(id == 1) cs1++;
+		else cs2++;			
+		schedule();		
+	}
+	return 0;
+}
+
+int init_module(void){
+	//initialize two kthreads and begin execution
+	t1 = kthread_create(incrementer, (void*)&id1, "inc1");
+	t2 = kthread_create(incrementer, (void*)&id2, "inc2");
+	if (t1 && t2){
+		printk(KERN_INFO "Starting..\n");
+		wake_up_process(t1);
+		wake_up_process(t2);
+	} else{
+		printk(KERN_EMERG "Error\n");
+	}
+	return 0;
+}
+
+void cleanup_module(void){
+	//initialize stat array to all zeros	
+	int i, x;
+	for(i = 0; i < 1000000; i++) stat[i] = 0;
+
+	//count the total number of entries in arr[i] who have been incremented to the value x
+	for(i = 0; i < 1000000; i++){
+		x = arr[i];
+		stat[x]++;
+	}
+
+	//print info on results of test.
+	for(i = 0; i < 1000000; i++){
+		if(stat[i] > 0) printk(KERN_INFO "i: %d x: %d\n",i,stat[i]);
+	}
+
+	printk(KERN_INFO "Thread 1 increment count: cs1 = %d\n",cs1);
+	printk(KERN_INFO "Thread 2 increment count: cs2 = %d\n",cs2);
+	printk(KERN_INFO "Combined thread increment count: cs1 + cs2 = %d\n",cs1+cs2);
+	printk(KERN_INFO "module unloaded succesfully.\n");
+}
+
+MODULE_LICENSE("GPL");
---
+++ Lab3-2.c
@@ -0,0 +1,74 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/kthread.h>
+#include<linux/semaphore.h>
+
+//two kthreads that will share access to data
+struct task_struct *t1, *t2;
+//semaphore to lock access to shared data
+struct semaphore lock;
+
+//
+int arr[1000000];
+int idx = 0;
+//total number of increments done by each kthread
+int id1 = 1, id2 = 2;
+int cs1 = 0, cs2 = 0;
+//array to total the number of single, double, triple, etc. increments that were done in arr
+int stat[1000000];
+
+int incrementer(void *ptr){
+	int tid = *(int*)ptr;
+	printk(KERN_INFO "Consumer TID %d\n", tid);
+	//increment every value of arr[idx] in each respective kthread if the semaphore is currently unlocked. 
+	//If it is, lock semaphore until arr[idx'] has been incremented.
+	while (idx < 1000000 && !kthread_should_stop()){
+		if(!down_interruptible(&lock)){
+			arr[idx++] += 1;
+			up(&lock);
+			if(tid == 1) cs1++;
+			else cs2++;			
+			schedule();		
+		}
+	}
+	return 0;
+}
+
+int init_module(void){
+	//initialize semaphore and two kthreads and begin execution
+	sema_init(&lock, 1);
+	t1 = kthread_create(incrementer, (void*)&id1, "inc1");
+	t2 = kthread_create(incrementer, (void*)&id2, "inc2");
+	if (t1 && t2){
+		printk(KERN_INFO "Starting..\n");
+		wake_up_process(t1);
+		wake_up_process(t2);
+	} else{
+		printk(KERN_EMERG "Error\n");
+	}
+	return 0;
+}
+
+void cleanup_module(void){
+	//initialize stat array to all zeros	
+	int i, x;
+	for(i = 0; i < 1000000; i++) stat[i] = 0;
+
+	//count the total number of entries in arr[i] who have been incremented to the value x
+	for(i = 0; i < 1000000; i++){
+		x = arr[i];
+		stat[x]++;
+	}
+
+	//print info on results of test.
+	for(i = 0; i < 1000000; i++){
+		if(stat[i] > 0) printk(KERN_INFO "i: %d x: %d\n",i,stat[i]);
+	}
+
+	printk(KERN_INFO "Thread 1 increment count: cs1 = %d\n",cs1);
+	printk(KERN_INFO "Thread 2 increment count: cs2 = %d\n",cs2);
+	printk(KERN_INFO "Combined thread increment count: cs1 + cs2 = %d\n",cs1+cs2);
+	printk(KERN_INFO "module unloaded succesfully.\n");
+}
+
+MODULE_LICENSE("GPL");
