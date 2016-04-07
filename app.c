cc09caa9271a6c387020f071b1ea8a58914bc271
AndrewAlbert <andrew.albert@att.net>
2016-04-07 12:37:11 -0400

    Finished lab6

---
+++ app.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#define FNAME "interface"
+
+int main(int argc, char **argv)
+{
+	int fd, length;
+	char read_buf[100], write_buf[100];
+
+	//ensure command line argument count is correct
+	if(argc > 3 || argc < 2){
+		printf("Invalid usage of app");
+		exit(0);
+	}
+	else if(argc == 3){
+		length = strlen(argv[2]);
+	}
+	else length = 0;
+
+	//open device
+	fd = open("/dev/interface", O_RDWR);
+	if(fd == -1){
+		printf("failed to open device\n");
+		exit(-1);
+	}
+
+	//read operation
+	if(!strcmp("read", argv[1])){
+		read(fd, read_buf, sizeof(read_buf));
+		printf("read buffer contents: %s\n", read_buf);
+	}
+	//write operation
+	else if(!strcmp("write", argv[1])){
+		if(length > 100){
+			printf("argument two is too long for write\n");
+			exit(0);
+		}
+		strcpy(write_buf, argv[2]);
+		write(fd, write_buf, sizeof(write_buf));
+		printf("write buffer contents: %s\n", write_buf);
+	}
+	else printf("invalid operation, use 'read' or 'write' in argument 1\n");
+
+	close(fd);
+	
+	return 0;
+}
--- readWriteSemaphore.c
+++ readWriteSemaphore.c
@@ -8,53 +8,59 @@
 
 MODULE_LICENSE("GPL");
 
-char array[100];
+//global array and read write semaphore
+int array[100];
 struct rw_semaphore rwsem;
 
+//relevant device driver info and work queue
 struct cdev *kernel_cdev;
 dev_t dev_no;
 int Major;
 wait_queue_head_t queue;
-int read_chars, written_chars;
 
 ssize_t read(struct file *filp, char *buff, size_t count, loff_t *offp){
-	printk(KERN_INFO "Read: start request\n");
 	unsigned long ret;
-	printk(KERN_INFO "Read: read = %d written = %d count = %d\n", read_chars, written_chars, (int)count);
+	printk(KERN_INFO "\nRead: start request\n");
+
 	down_read(&rwsem);
-	printk(KERN_INFO "Read: Arrived in critical section\n");
+	printk(KERN_INFO "\tRead arrived in critical section\n");
 	wait_event_timeout(queue, 0, 20*HZ);
-	printk(KERN_INFO "Read: start of copy\n");
+
+	printk(KERN_INFO "\tstart of copy to user\n");
 	ret = copy_to_user(buff, array, count);
-	printk(KERN_INFO "Read: end of the copy\n");
+	printk(KERN_INFO "\tend of copy to user\n");
+	printk(KERN_INFO "\tglobal array contents: %s\n", buff);
+
 	up_read(&rwsem);
-	printk(KERN_INFO "Read: semaphore released\n");
+	printk(KERN_INFO "\tsemaphore released, exiting read critical section\n");
 	return count;
 };
 
 ssize_t write(struct file *filp, const char *buff, size_t count, loff_t *offp){
-	printk(KERN_INFO "Write start request\n");
 	unsigned long ret;
-	down_write(&rwsem);
-	wait_event_timeout(queue, 0, 20*HZ);
-	if(count > 99) count = 99;
-	ret = copy_from_user(array, buff, count);
-	written_chars = count;
+	printk(KERN_INFO "\nWrite: start request\n");
 
-	printk(KERN_INFO "Write: read = %d written = %d count = %d\n", read_chars, written_chars, (int)count);
-	printk(KERN_INFO "Write buffer: %s\n", buff);
+	down_write(&rwsem);
+	printk(KERN_INFO "\tWrite arrived in critical section\n");
+	wait_event_timeout(queue, 0, 20*HZ);
+
+	printk(KERN_INFO "\tstart of copy from user\n");
+	ret = copy_from_user(array, buff, count);
+	printk(KERN_INFO "\tend of copy from user\n");
+	printk(KERN_INFO "\tglobal array contents: %s\n", buff);
+	
 	up_write(&rwsem);
+	printk(KERN_INFO "\tsemaphore released, exiting write critical section\n");
 	return count;
 };
 
 int open(struct inode *inode, struct file *filp){
-	printk(KERN_INFO "Open: read = %d written = %d\n", read_chars, written_chars);
-	read_chars = written_chars;
+	printk("\nOpened Device\n");
 	return 0;
 };
 
 int release(struct inode *inode, struct file *filp){
-	printk(KERN_INFO "Release: read = %d written = %d\n", read_chars, written_chars);
+	printk("\nReleased Device\n");
 	return 0;
 };
 
@@ -80,25 +86,30 @@ int init_module(void){
 		return ret;
 	}
 
+	//get device Major # and print for the user
 	Major = MAJOR(dev_no);
 	printk(KERN_INFO "Major number: %d\n", Major);
 	dev = MKDEV(Major, 0);
-	ret = cdev_add(kernel_cdev, dev, 1);
 
+	//register device and ensure it is properly allocated
+	ret = cdev_add(kernel_cdev, dev, 1);
 	if(ret < 0){
 		printk(KERN_ERR "Unable to allocate cdev\n");
 		return ret;
 	}
 
+	//initialize semaphore and workqueue
 	init_rwsem(&rwsem);
 	init_waitqueue_head(&queue);
+
 	printk(KERN_INFO "Module succesfully loaded\n");
+
 	return 0;
 };
 
 void cleanup_module(void){
-	printk(KERN_INFO "Module unloaded\n");
 	cdev_del(kernel_cdev);
 	unregister_chrdev_region(dev_no,1);
 	unregister_chrdev(Major,"interface");
+	printk(KERN_INFO "Module unloaded\n");
 }
